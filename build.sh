80abd227a91d5f372ada2f69365eb385014fe489
pinionpi <pinionpi@hotmail.com>
2021-05-31 16:04:40 +0700

    Merge build scripts to run at top-level project (git clone opus v1.3.1 inside)

--- build.sh
+++ build.sh
@@ -1,4 +1,19 @@
 #!/bin/bash
+
+# opus repo v1.3.1 from xiph
+git clone https://gitlab.xiph.org/xiph/opus
+cd opus && git checkout v1.3.1
+
+# build lib
+./autogen.sh
+emconfigure ./configure --disable-rtcd --disable-intrinsics --disable-shared \
+  --enable-static  --disable-stack-protector --disable-hardening
+emmake make -j$(nproc)
+
+# should output libopus.a in .libs/ that is linkable with emcc
+cd ..
+
+## build wasm
 # usage: ./build.sh [flags]
 # asm.js release flags: -O2 --memory-init-file 0
 # wasm release flags: -O2 -s WASM=1 -s ALLOW_MEMORY_GROWTH=1
